7741bba01e7ccd332ea9a96e574e2d42574ec204
Michael <m.a.schappacher@gmail.com>
2021-10-07 15:07:37 -0400

    Fix: Only install if not installed

--- install.sh
+++ install.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
 echo "--Starting personalize"
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-
 echo "--Installing cargo"
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+if [ -d "~/.cargo" ]; then
+    echo "rust already installed"
+else
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+fi
 
 echo "--Installing fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if [ -d "~/.fzf" ]; then
+    echo "fzf already installed"
+else
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
 
 echo "--Installing nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+if [ -d "~/.nvm" ]; then
+    echo "nvm already installed"
+else
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+fi
 
 echo "--Installing pyenv"
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+
+if [ -d "~/.pyenv" ]; then
+    echo "pyenv already installed"
+else
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+fi
+
 
 echo "--Installing ohmyzsh"
 sudo apt install zsh -y 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ -d "~/.oh-my-zsh" ]; then
+    echo "ohmyzsh already installed"
+else
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    rm ~.zshrc
+fi
+
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
